52575d310d2b31ac61d63f1610375d3a3ba22026
Andrew Burdak <burdak1994@gmail.com>
2025-11-04 09:31:34 +0300

    add: new class FuncReferencExercise

---
+++ src/src/FifthSprint/FilesExercises/FilesExercises.java
@@ -0,0 +1,24 @@
+package FifthSprint.FilesExercises;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FilesExercises {
+    private static final String HOME = System.getProperty("user.home");
+
+    public static void main(String[] args) {
+        Path testFilePath = Paths.get(HOME, "test", "file.txt");
+
+        System.out.println("Information about file: ");
+        System.out.println(" - name: " + testFilePath.getFileName());
+        System.out.println(" - root directory: " + testFilePath.getRoot());
+        System.out.println(" - parent directory: " + testFilePath.getParent());
+
+        System.out.println("Elements of path: ");
+        for (Path path : testFilePath) {
+            System.out.println(" - element of path: " + path);
+        }
+    }
+}
